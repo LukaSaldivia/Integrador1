ba4ba6df460e3ba5f11b9048bf2b924e3f5234e9
Maximo Bayones <bayonesmaximo20@gmail.com>
2025-09-06 21:22:42 -0300

    metodos correspondientes a la tabla facturas

--- src/main/java/ar/unicen/exa/aldesal/repository/mysql/MySQLDAOFactura.java
+++ src/main/java/ar/unicen/exa/aldesal/repository/mysql/MySQLDAOFactura.java
@@ -1,4 +1,69 @@
 package ar.unicen.exa.aldesal.repository.mysql;
 
-public class MySQLDAOFactura {
+import ar.unicen.exa.aldesal.dao.FacturaDAO;
+import ar.unicen.exa.aldesal.entity.Factura;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLDAOFactura extends MySQLEntidadDAO implements FacturaDAO {
+    public MySQLDAOFactura(Connection connection) {
+        super(connection);
+    }
+
+    @Override
+    public void insertarVarios(List<Factura> EntidadDAO) {
+        String sql =
+            """
+            INSERT INTO factura (idFactura, idCliente) VALUES
+            """;
+
+        ArrayList<String> values = new ArrayList<>(), placeholder = new ArrayList<>();
+
+        for (Factura Entidad : EntidadDAO) {
+            placeholder.add("(?,?)");
+            values.add(String.valueOf(Entidad.getIdFactura()));
+            values.add(String.valueOf(Entidad.getIdCliente()));
+        }
+
+        sql += String.join(",", placeholder);
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            for (Factura Entidad : EntidadDAO) {
+                ps.setInt(1, Entidad.getIdFactura());
+                ps.setInt(2, Entidad.getIdCliente());
+                ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public void vaciar() {
+        String sql = "DELETE FROM factura";
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    protected String getInitQuery() {
+        return
+                """
+                CREATE TABLE IF NOT EXISTS Factura (
+                idFactura INTEGER PRIMARY KEY,
+                idCliente INTEGER NOT NULL
+                )
+                """;
+    }
 }
