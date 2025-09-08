fe1577c63ca87630e1dd5f8503c3063a981744d6
Maximo Bayones <bayonesmaximo20@gmail.com>
2025-09-08 18:33:41 -0300

    actualizada la password

--- src/main/java/ar/unicen/exa/aldesal/factory/MySQLConnectionManager.java
+++ src/main/java/ar/unicen/exa/aldesal/factory/MySQLConnectionManager.java
@@ -13,7 +13,7 @@ public final class MySQLConnectionManager {
     // Configuración
     private static final String URL = "jdbc:mysql://localhost:3306/db_integrador_1";
     private static final String USER = "root";
-    private static final String PASSWORD = "password";
+    private static final String PASSWORD = "";
 
     private MySQLConnectionManager() {
         try {
